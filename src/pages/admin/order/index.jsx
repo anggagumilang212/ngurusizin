c9ad6ddb9665b5318766acc5960ee1002b40f842
R3N-di <rendiramadhanmars@gmail.com>
2024-03-13 23:11:08 +0700

    menambahkan loading,

--- src/pages/admin/order/index.jsx
+++ src/pages/admin/order/index.jsx
@@ -47,17 +47,8 @@ const Order = ({ isLoggedIn }) => {
   // console.log('indexOrder', CekRole());
   const fetchData = async () => {
     let url = '';
-    // const roleCari = CekRole();
 
     try {
-      // gak konsisten nilai nya, bener kalo saat dicari
-      // if(role == 'merchant'){
-      //   url = `http://localhost:5000/api/order?page=${currentPage}&role=merchant`
-      // }
-      // else{
-      //   url = `http://localhost:5000/api/order?page=${currentPage}&role=` 
-      // }
-
       const response = await axios.get(`http://localhost:5000/api/order?page=${currentPage}&role=${role}`);
 
       console.log(response);
@@ -68,8 +59,6 @@ const Order = ({ isLoggedIn }) => {
     } catch (error) {
       console.error("Error fetching data order:", error);
       setError(error);
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -89,19 +78,17 @@ const Order = ({ isLoggedIn }) => {
     } catch (error) {
       console.error("Error fetching data order:", error);
       setError(error);
-    } finally {
-      setLoading(false);
     }
   };
 
   // untuk mengatasi error yang tidak jelas
-  // 
   useEffect(() => {
     let nomor = 0;
     // kondisi search
     if (nomor == 0) {
       setSearchAndClearAfterDelay('  ', 500);
       nomor++;
+      setLoading(false);
     }
   }, [])
 
@@ -291,74 +278,82 @@ const Order = ({ isLoggedIn }) => {
                     </tr>
                   </thead>
                   <tbody>
-                    {order.map((item) => (
-                      <tr
-                        className="border-b dark:border-neutral-500"
-                        key={item.id}
-                      >
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {item.attributes.layananId}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {item.attributes.nama}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {item.attributes.domisili}
-                        </td>
-
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          {item.attributes.phone}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <button
-                            onClick={() => handleEdit(item)}
-                            className="relative inline-block p-px text-xs font-semibold leading-6 text-white no-underline rounded-full shadow-2xl cursor-pointer bg-slate-800 group shadow-zinc-900"
-                          >
-                            <span className="absolute inset-0 overflow-hidden rounded-full">
-                              <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-                            </span>
-                            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-                              <span>{item.attributes.status}</span>
-                              <svg
-                                fill="none"
-                                height="16"
-                                viewBox="0 0 24 24"
-                                width="16"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  d="M10.75 8.75L14.25 12L10.75 15.25"
-                                  stroke="currentColor"
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  strokeWidth="1.5"
-                                />
-                              </svg>
-                            </div>
-                            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-                          </button>
-                        </td>
-
-                        <td className="flex items-center gap-1 px-6 py-4 mt-8 whitespace-nowrap">
-                          {/* button delete */}
-                          <button
-                            onClick={() => {
-                              toggleModalDelete();
-                              setItemIdToDelete(item.id);
-                              // Simpan ID item yang akan dihapus
-                            }}
-                            className="items-center w-auto px-5 py-2 mb-2 tracking-wider text-white rounded-full shadow-sm bg-gradient-to-r from-indigo-400 to-gray-600 md:mb-0 hover:bg-gray-800"
-                            aria-label="delete"
-                          >
-                            {isDeleting ? (
-                              "Menghapus..."
-                            ) : (
-                              <i className="fa-solid fa-trash"></i>
-                            )}
-                          </button>
-                        </td>
+                    {loading ? (
+                      <tr>
+                        <td colSpan={6} className="text-center">Loading ...</td>
                       </tr>
-                    ))}
+                    ) : (
+                      <>
+                      {order.map((item) => (
+                        <tr
+                          className="border-b dark:border-neutral-500"
+                          key={item.id}
+                        >
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            {item.attributes.layanan.nama}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            {item.attributes.nama}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            {item.attributes.domisili}
+                          </td>
+  
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            {item.attributes.phone}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap">
+                            <button
+                              onClick={() => handleEdit(item)}
+                              className="relative inline-block p-px text-xs font-semibold leading-6 text-white no-underline rounded-full shadow-2xl cursor-pointer bg-slate-800 group shadow-zinc-900"
+                            >
+                              <span className="absolute inset-0 overflow-hidden rounded-full">
+                                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                              </span>
+                              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                                <span>{item.attributes.status}</span>
+                                <svg
+                                  fill="none"
+                                  height="16"
+                                  viewBox="0 0 24 24"
+                                  width="16"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    d="M10.75 8.75L14.25 12L10.75 15.25"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="1.5"
+                                  />
+                                </svg>
+                              </div>
+                              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+                            </button>
+                          </td>
+  
+                          <td className="flex items-center gap-1 px-6 py-4 mt-8 whitespace-nowrap">
+                            {/* button delete */}
+                            <button
+                              onClick={() => {
+                                toggleModalDelete();
+                                setItemIdToDelete(item.id);
+                                // Simpan ID item yang akan dihapus
+                              }}
+                              className="items-center w-auto px-5 py-2 mb-2 tracking-wider text-white rounded-full shadow-sm bg-gradient-to-r from-indigo-400 to-gray-600 md:mb-0 hover:bg-gray-800"
+                              aria-label="delete"
+                            >
+                              {isDeleting ? (
+                                "Menghapus..."
+                              ) : (
+                                <i className="fa-solid fa-trash"></i>
+                              )}
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                      </>
+                    )}
                   </tbody>
                 </table>
 
